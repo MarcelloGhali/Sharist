4aac8b2e8d323136b8836e3b305df824a91689c8
unknown <v-maratg@galeyev-macpro.redmond.corp.microsoft.com>
2015-08-09 21:37:59 -0700

    new tests added

--- costsplitter/costsplitter.test/costsplitter.test.cpp
+++ costsplitter/costsplitter.test/costsplitter.test.cpp
@@ -35,5 +35,33 @@ BOOST_AUTO_TEST_CASE(sharedevent_should_expand_if_new_members_added){
 	BOOST_CHECK(countResults*oregon.GetGrowthRate() == countExpandedResults);
 }
 
+BOOST_AUTO_TEST_CASE(sharedevent_should_return_correct_optimization){
+	//setup
+	Member marat("Marat");
+	Member alex("Alex");
+	Member slava("Slava");
+	vector<const Member*> maratMembers;
+	maratMembers.push_back(&alex);
+	maratMembers.push_back(&slava);
+	vector<const Member*> slavaMembers;
+	slavaMembers.push_back(&alex);
+	slavaMembers.push_back(&marat);
+	ExpenseItem gas(90, &marat, 0, &maratMembers);
+	ExpenseItem food(120, &slava, 0, &slavaMembers);
+	SharedEvent oregon;
+	oregon.AddMember(&marat);
+	oregon.AddMember(&alex);
+	oregon.AddMember(&slava);
+	oregon.AddExpenseItem(&gas);
+	oregon.AddExpenseItem(&food);
+	//expected
+	double alexOweSlava = 50;
+	double alexOweMarat = 20;
+	//test
+	double** results = oregon.Optimize();
+	//adding new users
+	BOOST_CHECK(results[1][0] == alexOweMarat);
+	BOOST_CHECK(results[1][2] == alexOweSlava);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
