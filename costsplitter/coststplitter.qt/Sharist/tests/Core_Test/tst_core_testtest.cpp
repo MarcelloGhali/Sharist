145e5934bfff1f87044fec3966b91c4ee5f066e6
Galeyev Marat <galeyevmarat@Galeyevs-MacBook-Pro.local>
2015-08-21 00:03:54 -0700

    merging tests

--- costsplitter/coststplitter.qt/Sharist/tests/Core_Test/tst_core_testtest.cpp
+++ costsplitter/coststplitter.qt/Sharist/tests/Core_Test/tst_core_testtest.cpp
@@ -11,6 +11,7 @@ public:
 
 private Q_SLOTS:
     void sharedevent_should_expand_if_new_members_added();
+    void sharedevent_should_return_correct_optimization();
 };
 
 Core_Test::Core_Test()
@@ -50,6 +51,35 @@ void Core_Test::sharedevent_should_expand_if_new_members_added()
     }
 
     delete tmpMembers;
+}
+
+void Core_Test::sharedevent_should_return_correct_optimization(){
+    //setup
+        Member marat("Marat");
+        Member alex("Alex");
+        Member slava("Slava");
+        vector<const Member*> maratMembers;
+        maratMembers.push_back(&alex);
+        maratMembers.push_back(&slava);
+        vector<const Member*> slavaMembers;
+        slavaMembers.push_back(&alex);
+        slavaMembers.push_back(&marat);
+        ExpenseItem gas(90, &marat, 0, &maratMembers);
+        ExpenseItem food(120, &slava, 0, &slavaMembers);
+        SharedEvent oregon;
+        oregon.AddMember(&marat);
+        oregon.AddMember(&alex);
+        oregon.AddMember(&slava);
+        oregon.AddExpenseItem(&gas);
+        oregon.AddExpenseItem(&food);
+        //expected
+        double alexOweSlava = 50;
+        double alexOweMarat = 20;
+        //test
+        double** results = oregon.Optimize();
+        //adding new users
+        QVERIFY(results[1][0] == alexOweMarat);
+        QVERIFY(results[1][2] == alexOweSlava);
 }
 
 QTEST_APPLESS_MAIN(Core_Test)
